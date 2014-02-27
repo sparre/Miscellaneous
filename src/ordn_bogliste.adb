b84bfd8522958cd7731f8f861f3ba779617eabda
Jacob Sparre Andersen <jacob@jacob-sparre.dk>
2014-02-27 15:49:17 +0100

    Tool for cleaning up lists of books.
    --HG--
    rename : src/ordn_filmliste.adb => src/ordn_bogliste.adb

---
+++ src/ordn_bogliste.adb
@@ -0,0 +1,39 @@
+with
+  Ada.Text_IO;
+with
+  Internet.Headers;
+
+procedure Ordn_Bogliste is
+   type Entry_Fields is (Titel, Originaltitel, Oversætter,
+                         Anskaffelsesdato, Anskaffelsespris,
+                         Udgivelsesår,
+                         Sprog,
+                         Sidetal,
+                         Pris,
+                         Værdi, Noter);
+
+   package Entry_Text_IO is new Internet.Headers (Entry_Fields);
+
+   use Ada.Text_IO;
+   use Entry_Text_IO;
+
+   Book : Header_Type;
+begin
+   loop
+      Get (File                     => Standard_Input,
+           Item                     => Book,
+           Skip_Leading_Empty_Lines => True);
+      Put (File => Standard_Output,
+           Item => Book);
+   end loop;
+exception
+   when End_Error =>
+      Put (File => Standard_Output,
+           Item => Book);
+   when others =>
+      Put_Line (File => Standard_Error,
+                Item => "An exception was raised in procedure " &
+                        "Ordn_Bogliste. At line " &
+                        Line (Standard_Input)'Img & " in the input data.");
+      raise;
+end Ordn_Bogliste;
